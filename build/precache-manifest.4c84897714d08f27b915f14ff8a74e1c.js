482894550f10690129ae0b7934c8469dd1f2273a
Namrata Dharurkar <dnamrata0805@gmail.com>
2019-10-14 19:38:39 +0530

    build remmoved from .gitignore

---
+++ build/precache-manifest.4c84897714d08f27b915f14ff8a74e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e219e169d27c8dbe4918e8571c77766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf79ef60586a86608fd",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fb644e8f84925080105c",
+    "url": "/static/css/main.a67538c2.chunk.css"
+  },
+  {
+    "revision": "fbf79ef60586a86608fd",
+    "url": "/static/js/2.0e82e9d1.chunk.js"
+  },
+  {
+    "revision": "fb644e8f84925080105c",
+    "url": "/static/js/main.f8074bc0.chunk.js"
+  },
+  {
+    "revision": "f5ce22c17382fc1d1460",
+    "url": "/static/js/runtime-main.6f7e06e2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
